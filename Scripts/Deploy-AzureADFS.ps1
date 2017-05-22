a6ef7655f47b8d3275bee2808b6c1a83f8da4d92
Stéphane Goudeau <stephgou@microsoft.com>
2017-05-22 16:38:28 +0200

    first commit

---
+++ Scripts/Deploy-AzureADFS.ps1
@@ -0,0 +1,57 @@
+﻿Param(  
+    # Name of the subscription to use for azure cmdlets
+    $subscriptionName = "stephgou - External",
+    $subscriptionId = "fb79eb46-411c-4097-86ba-801dca0ff5d5",
+    #Paramètres du Azure Ressource Group
+    $resourceGroupName = "SG-RG-AzureADFS",
+    $resourceGroupDeploymentName = "AzureADFS-Deployed",
+    $resourceLocation = "West Europe",
+    $templateFile = "azuredeploy.json",
+    $templateParameterFile = "azuredeploy.parameters.json",
+    $templateFolder = "..\Templates",
+    $tagName = "SG-RG-ADFS",
+    $tagValue = "DSC"
+    )
+
+#region init
+Set-PSDebug -Strict
+
+#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Network).ResourceTypes | Where-Object ResourceTypeName -eq virtualNetworks).ApiVersions
+#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Network).ResourceTypes | Where-Object ResourceTypeName -eq LoadBalancers).ApiVersions
+#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Network).ResourceTypes | Where-Object ResourceTypeName -eq publicIPAddresses).ApiVersions
+#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Network).ResourceTypes | Where-Object ResourceTypeName -eq virtualNetworkGateways).ApiVersions
+#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Network).ResourceTypes | Where-Object ResourceTypeName -eq networkInterfaces).ApiVersions
+#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Compute).ResourceTypes | Where-Object ResourceTypeName -eq virtualMachines).ApiVersions
+#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Compute).ResourceTypes | Where-Object ResourceTypeName -eq availabilitySets).ApiVersions
+#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Resources).ResourceTypes | Where-Object ResourceTypeName -eq deployments).ApiVersions
+#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Storage).ResourceTypes | Where-Object ResourceTypeName -eq storageAccounts).ApiVersions
+
+cls
+$d = get-date
+Write-Host "Starting Deployment $d"
+
+$scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Write-Host "scriptFolder" $scriptFolder
+
+set-location $scriptFolder
+#endregion init
+
+#Login-AzureRmAccount -SubscriptionId $subscriptionId
+
+# Resource group create
+New-AzureRmResourceGroup `
+	-Name $resourceGroupName `
+	-Location $resourceLocation `
+    -Tag @{Name=$tagName;Value=$tagValue} `
+    -Verbose
+
+# Resource group deploy
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceGroupDeploymentName `
+	-ResourceGroupName $resourceGroupName `
+	-TemplateFile "$scriptFolder\$templatefolder\$templateFile" `
+	-TemplateParameterFile "$scriptFolder\$templatefolder\$templateParameterFile" `
+    -Debug -Verbose -DeploymentDebugLogLevel All
+
+$d = get-date
+Write-Host "Stopping Deployment $d"
